001a938061e34ad4a34461eb7473c4d318113139
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-05-24 13:33:33 -0300

    config backend

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class UsersModel(models.Model):
+    nome = models.CharField(verbose_name='Nome Empresa / Pessoal', max_length=100, unique=True)
+    email = models.EmailField(verbose_name='Email', max_length=100, unique=True)
+    whatsapp = models.CharField(verbose_name='WhatsApp', max_length=100)
+    senha = models.CharField(verbose_name='Senha', max_length=200)
+    licenca = models.CharField(verbose_name="Licença", default='gratuita', null=True)
+
+
+
